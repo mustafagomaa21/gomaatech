edfe3bf485e405d45e5280c7b51d848fcd728b77
Mustafa Gomaa <40192736+mustafagomaa21@users.noreply.github.com>
2019-02-06 17:41:55 +0200

    Update and rename download.css to download.js

---
+++ download.js
@@ -0,0 +1,27 @@
+&lt;script type=&#039;text/javascript&#039;&gt;
+var uri = window.location.toString();
+if (uri.indexOf(&quot;%3D&quot;,&quot;%3D&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;%3D&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;%3D%3D&quot;,&quot;%3D%3D&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;%3D%3D&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;&amp;m=1&quot;,&quot;&amp;m=1&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;&amp;m=1&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;?m=1&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+&lt;/script&gt;
+
+&lt;script type=&#039;text/javascript&#039;&gt;
+var str= window.location.href.toString(); if((str.indexOf(&amp;#39;.com/&amp;#39;))==&amp;#39;-1&amp;#39;) { var str1=str.substring(str.lastIndexOf(&amp;quot;.blogspot.&amp;quot;));if (str1.indexOf(&amp;#39;/&amp;#39;)==&amp;#39;-1&amp;#39;) { var str2=str1; } else { var
+str2=str1.substring(0,str1.indexOf(&amp;#39;/&amp;#39;)+1); } window.location.href=window.location.href.toString().replace(str2,&amp;#39;.blogspot.com/ncr/&amp;#39;); }
+&lt;/script&gt;
